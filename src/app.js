b7c496ec2e88ae2bac2cf2e11fd6845df6d789d5
Thakur Saad <thakursaad613@gmail.com>
2024-11-28 11:27:19 +0600

    first

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const globalErrorHandler = require("./app/middleware/globalErrorHandler");
+const routes = require("./app/routes");
+const NotFoundHandler = require("./error/NotFoundHandler");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/", routes);
+
+app.get("/", async (req, res) => {
+  res.json("Welcome to TrackTrek");
+});
+
+app.use(globalErrorHandler);
+app.use(NotFoundHandler.handle);
+
+module.exports = app;
---
+++ src/app/middleware/fileUploader.js
@@ -0,0 +1,58 @@
+const multer = require("multer");
+const fs = require("fs");
+
+const allowedMimeTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
+
+const isValidFileType = (mimetype) => allowedMimeTypes.includes(mimetype);
+
+const createDirIfNotExists = (uploadPath) => {
+  if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath, { recursive: true });
+};
+
+const uploadFile = () => {
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      let uploadPath = "";
+
+      if (file.fieldname === "profile_image")
+        uploadPath = "uploads/images/profile";
+      else uploadPath = "uploads";
+
+      createDirIfNotExists(uploadPath);
+
+      if (isValidFileType(file.mimetype)) {
+        cb(null, uploadPath);
+      } else {
+        cb(new Error("Invalid file type"));
+      }
+    },
+    filename: function (req, file, cb) {
+      const name = Date.now() + "-" + file.originalname;
+      cb(null, name);
+    },
+  });
+
+  const fileFilter = (req, file, cb) => {
+    const allowedFieldNames = ["profile_image"];
+
+    // Allow requests without files (when there's no fieldname)
+    if (!file.fieldname) return cb(null, true);
+
+    // Check if the fieldname is valid
+    if (!allowedFieldNames.includes(file.fieldname))
+      return cb(new Error("Invalid fieldname"));
+
+    // Check if the file type is valid
+    if (isValidFileType(file.mimetype)) return cb(null, true);
+    else return cb(new Error("Invalid file type"));
+  };
+
+  const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+  }).fields([{ name: "profile_image", maxCount: 1 }]);
+
+  return upload;
+};
+
+module.exports = { uploadFile };
