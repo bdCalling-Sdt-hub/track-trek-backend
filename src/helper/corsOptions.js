fcc3b88e384b4af873dd11610d5410680d837a5c
ThakurSaad <thakursaad613@gmail.com>
2025-02-03 15:40:13 +0600

    update cors

--- src/helper/corsOptions.js
+++ src/helper/corsOptions.js
@@ -7,6 +7,8 @@ const corsOptions = {
     "http://10.0.60.26:8080",
     "http://localhost:5173",
     "http://10.0.60.24:5173",
+    "http://10.0.60.24:3003",
+    "http://209.97.134.184:8002",
   ],
   credentials: true,
 };
